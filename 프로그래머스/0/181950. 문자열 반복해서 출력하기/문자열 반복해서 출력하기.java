e7512a4f98bb4820fbacbbd0ae2ec78760fe8fc8
남홍근 <ghdrms1220@naver.com>
2024-08-16 14:25:10 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 146.30 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        String temp = str.repeat(n);
+        System.out.printf(temp);
+    }
+}
