aba81c6c947e4fbabaccbb1051c3babbedf6fbd3
남홍근 <ghdrms1220@naver.com>
2024-08-16 14:59:50 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 1.72 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        int temp = 0;
+        answer += my_string.substring(0,s);
+        answer += overwrite_string.substring(0,overwrite_string.length());
+        answer+= my_string.substring(answer.length(),my_string.length());
+        return answer;
+    }
+}
