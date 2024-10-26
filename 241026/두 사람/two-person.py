c9a13f690162a2c1742f55b6fbefb734c30cc97e
남홍근 <ghdrms1220@naver.com>
2024-10-26 13:27:39 +0900

    Title: 두 사람, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241026/두 사람/two-person.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+age = []
+sex = []
+first_age, first_sex = list(map(str,input().split()))
+second_age, second_sex = list(map(str,input().split()))
+age.append(int(first_age))
+age.append(int(second_age))
+sex.append(first_sex)
+sex.append(second_sex)
+IsMale = False
+for i in range(2):
+
+    if int(age[i] )>=19 and sex[i] == 'M':
+        IsMale = True
+        break
+    else:
+        print(0)
+
+print(1 if IsMale else 0)
