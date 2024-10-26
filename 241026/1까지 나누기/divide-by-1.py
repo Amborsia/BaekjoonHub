35705dcb5f2ed7d16ba63a609113ab7a51145d54
남홍근 <ghdrms1220@naver.com>
2024-10-26 14:30:08 +0900

    Title: 1까지 나누기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241026/1까지 나누기/divide-by-1.py
+++ 241026/1까지 나누기/divide-by-1.py
@@ -3,9 +3,10 @@ input = sys.stdin.readline
 
 a = int(input().rstrip())
 count = 0
-while True:
-    count+=1
-    a //=count
-    if a <=0:
-        break
-print(count)
+
+while a > 0:
+    count += 1  
+    a //= count 
+    
+
+print(count - 1)
