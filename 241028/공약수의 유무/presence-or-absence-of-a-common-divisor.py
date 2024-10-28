8d327b8c0cdbed47af0a4967891cf1b067483043
남홍근 <ghdrms1220@naver.com>
2024-10-28 11:31:30 +0900

    Title: 공약수의 유무, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241028/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 241028/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,12 +1,22 @@
 import sys
 input = sys.stdin.readline
-
-IsFinish = False
 a,b = list(map(int,input().split()))
-for i in range(1,a):
-    if a%i == 0 and b %i == 0:
-        print(1)
-        IsFinish = True
+lit_a = []
+lit_b = []
+for i in range(1,a+1):
+    if a%i == 0:
+        lit_a.append(i)
+for i in range(1,b+1):
+    if a%i == 0:
+        lit_b.append(i)
+
+max_len = len(lit_b) if len(lit_b)>=len(lit_a) else len(lit_a)
+IsFinish = False
+for i in range(max_len):
+    if lit_b[i] in lit_a:
+        IsFinish =True
         break
 if IsFinish == False:
     print(0)
+else:
+    print(1)
