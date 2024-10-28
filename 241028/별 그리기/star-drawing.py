7bd07b76509fa2a87282697979d6f4151a62f7cd
남홍근 <ghdrms1220@naver.com>
2024-10-28 14:00:03 +0900

    Title: 별 그리기, Time: 186ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 241028/별 그리기/star-drawing.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(' ',end= '')
+    for j in range(2*i-1):
+        print('*',end='')
+    for j in range(n-i):
+        print(' ',end= '')
+    print()
+
+for i in range(n - 1, 0, -1):
+    for j in range(n - i):
+        print(' ', end='')
+    for j in range(2 * i - 1):
+        print('*', end='')
+    print()
