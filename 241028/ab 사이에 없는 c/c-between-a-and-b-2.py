d6ec9855842e6bbecc28c7e573f7bec2d69fbdb4
남홍근 <ghdrms1220@naver.com>
2024-10-28 11:41:10 +0900

    Title: ab 사이에 없는 c, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241028/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+a,b,c = list(map(int,input().split()))
+IsFinish = False
+for i in range(1,b+1):
+    if i%c == 0:
+        IsFinish = True
+        break
+
+if IsFinish:
+    print("NO")
+else:
+    print("YES")
