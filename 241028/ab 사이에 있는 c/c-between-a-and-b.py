1446a00e7e6c249c9fdd34af1bcd732e9d6777f8
남홍근 <ghdrms1220@naver.com>
2024-10-28 11:34:22 +0900

    Title: ab 사이에 있는 c, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241028/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+a,b,c = list(map(int,input().split()))
+IsFinish = False
+for i in range(a,b+1):
+    if i%c == 0:
+        IsFinish = True
+        break
+
+if IsFinish:
+    print("YES")
+else:
+    print("NO")
