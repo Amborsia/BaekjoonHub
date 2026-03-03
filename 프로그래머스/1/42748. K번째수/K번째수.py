91bf3a9c5ac2f0d071784c0472a1b2980307f35f
남홍근 <ghdrms1220@naver.com>
2026-03-03 15:18:42 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.05 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for i,j,k in commands:
+        temp = array[i-1:j]
+        temp = sorted(temp)
+        answer.append(temp[k-1])
+        
+    return answer
