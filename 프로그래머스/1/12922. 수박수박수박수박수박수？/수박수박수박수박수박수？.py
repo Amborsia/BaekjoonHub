00438d9e7d810ccbde01e565b3671aa5522a2989
남홍근 <ghdrms1220@naver.com>
2026-03-03 12:58:44 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.64 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,12 +1,11 @@
 def solution(n):
     answer = ''
-    if n == 0:
-        return ""
-    elif n == 1:
-        return "수"
-    else:
-        if(n %2 == 0):
-            return "수박"*(n//2)
+    idx = 0
+    while n !=0:
+        if idx%2==0:
+            answer+="수"
         else:
-            return "수박"*(n//2)+"수"
-    
+            answer+="박"
+        n-=1
+        idx+=1
+    return answer
