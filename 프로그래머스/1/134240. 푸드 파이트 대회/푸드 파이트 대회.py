a7a207fd66f9df6dbdf9db4ed22458477dbe4c9b
남홍근 <ghdrms1220@naver.com>
2026-03-03 14:50:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 4.40 ms, Memory: 9.58 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,13 +1,20 @@
 def solution(food):
-    answer = ''
+    answer = 0
+    temp = 0
     for i in range(1,len(food)):
-        answer+=str(i)*int(food[i]//2)
-
-    temp = list(answer)
-    temp.reverse()
-    answer+='0'
-    for i in temp:
-        answer+=str(i)
-    
-        
-    return answer
+        if food[i]%2 == 1:
+            temp += food[i]-1
+        else:
+            temp+=food[i]
+    answer = ["0" for _ in range(temp+1)]
+    temp = temp//2
+    start,end = 0,len(answer)-1
+    for i in range(1,len(food)):
+        while food[i] >=2:
+            answer[start] = str(i)
+            answer[end] = str(i)
+            start +=1
+            end-=1
+            food[i] -=2
+            
+    return "".join(answer)
