8bdf825585d435590c2706d0a1e2778307d52e3b
남홍근 <ghdrms1220@naver.com>
2026-03-03 15:49:45 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.81 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,16 +1,12 @@
+import heapq
 def solution(k, score):
     answer = []
-    ranking = []
+    temp = []
     for i in score:
-        if len(ranking) < k:
-            ranking.append(i)
-        else:
-            minv = min(ranking)
-            # 10,20,100 일때 150이 들ㅇ오면 10 삭제해야함
-            if minv <= i:
-                temp = ranking.index(minv)
-                ranking[temp]= i
-        # print(ranking)
-        answer.append(min(ranking))
-            
+        heapq.heappush(temp,i)
+        
+        if len(temp) >k:
+            heapq.heappop(temp)
+        answer.append(temp[0])
+        
     return answer
