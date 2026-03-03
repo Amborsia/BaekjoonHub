484fdfb200d03bf8fd9ed789041dad45d72d0dc5
남홍근 <ghdrms1220@naver.com>
2026-03-03 16:41:07 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,13 +1,17 @@
 def solution(n, arr1, arr2):
     answer = []
-    for i in range(n):
-        line1 = bin(arr1[i])[2:].zfill(n)
-        line2 = bin(arr2[i])[2:].zfill(n)
-        line =''
-        for j in range(n):
-            if(line1[j] == '1' or line2[j] == '1'):
-                line+='#'
-            else:
-                line+=' '
-        answer.append(line)
-    return answer
+    # print(bin(2))
+    result1 = []
+    result2 = []
+    result = []
+    for i in range(0,n):
+        val1 = int(format(arr1[i], f'0{n}b'))
+        val2 = int(format(arr2[i], f'0{n}b'))
+        result.append(val1 + val2)
+    # print(result)
+    for i in result:
+        s = str(i).zfill(n)
+        result1.append(s.replace('0',' ').replace('1','#').replace('2','#')
+                      )
+    
+    return result1
