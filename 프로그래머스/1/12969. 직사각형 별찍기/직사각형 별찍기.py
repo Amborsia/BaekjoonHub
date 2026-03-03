10bbd6f544f2ff585e52acc4bdd760bc293452e7
남홍근 <ghdrms1220@naver.com>
2026-03-03 13:21:16 +0900

    [level 1] Title: 직사각형 별찍기, Time: 31.93 ms, Memory: 7.57 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.py
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -1,7 +1,5 @@
 a, b = map(int, input().strip().split(' '))
-temp = ''
 for i in range(0,b):
     for j in range(0,a):
-        temp+='*'
-    temp+='\n'
-print(temp)
+        print('*',end='')
+    print()
