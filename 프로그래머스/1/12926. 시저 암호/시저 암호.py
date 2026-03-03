dd9cd9a8f62a774b0c6c40d9df5dbf372528cbad
남홍근 <ghdrms1220@naver.com>
2026-03-03 14:31:56 +0900

    [level 1] Title: 시저 암호, Time: 3.17 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,17 +1,15 @@
 def solution(s, n):
-    set_lit = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    set_big_lit = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     answer = ''
-    temp_lit = list(str(s))
-    for i in temp_lit:
-        temp = 0
-        if(i.isupper()):
-            temp = set_big_lit.index(i)
-            answer += set_big_lit[(temp+n)%26]
-            
-        elif(i.islower()):
-            temp = set_lit.index(i)
-            answer += set_lit[(temp+n)%26]
-        else:
-            answer += ' '
+    for i in range(0,len(s)):
+        temp = ord(s[i])
+        if 65<=temp<=90:
+            temp+=n
+            if temp >90:
+                temp -= 26
+        elif 97<=temp<=122:
+            temp+=n
+            if temp >122:
+                temp -= 26
+        answer += chr(temp)
+        # print(chr(ord(s[i])+1))
     return answer
