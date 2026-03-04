6101abe9ca3921d75df3086b8756477981af633a
남홍근 <ghdrms1220@naver.com>
2026-03-04 11:53:56 +0900

    [level 1] Title: 모의고사, Time: 2.46 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,20 +1,30 @@
 def solution(answers):
-    temp1 = [1,2,3,4,5]
-    temp2 = [2,1,2,3,2,4,2,5]
-    temp3 = [3,3,1,1,2,2,4,4,5,5]
-    scores = [0, 0, 0]
+    answer = []
+    num1 = [1,2,3,4,5]
+    num2 = [2,1,2,3,2,4,2,5]
+    num3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0,0,0]
+    num1_limit = len(num1)
+    num1_current = 0
+    num1_correct = 0
+    num2_limit = len(num2)
+    num2_current = 0
+    num2_correct = 0
+    num3_limit = len(num3)
+    num3_current = 0
+    num3_correct = 0
+    for i in range(len(answers)):
+        if answers[i] == num1[i%num1_limit]:
+            score[0] +=1
+        if answers[i] == num2[i%num2_limit]:
+            score[1] +=1
+        if answers[i] == num3[i%num3_limit]:
+            score[2] +=1
+    maxs = max(score)
+    for i in range(3):
+        if score[i] == maxs:
+            answer.append(i+1)
+    return answer
+  
+        
     
-    # 정답을 하나씩 체크
-    for i, answer in enumerate(answers):
-        if answer == temp1[i % len(temp1)]:
-            scores[0] += 1
-        if answer == temp2[i % len(temp2)]:
-            scores[1] += 1
-        if answer == temp3[i % len(temp3)]:
-            scores[2] += 1
-    max_score = max(scores)
-    result = []
-    for i, score in enumerate(scores):
-        if(score == max_score):
-            result.append(i+1)
-    return result
