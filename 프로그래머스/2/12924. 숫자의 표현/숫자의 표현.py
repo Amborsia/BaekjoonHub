d211ce6e44f2bcedc7bac7a3e639f0f553ef1ed5
남홍근 <ghdrms1220@naver.com>
2026-03-05 15:30:32 +0900

    [level 2] Title: 숫자의 표현, Time: 0.42 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,15 +1,6 @@
 def solution(n):
-    count = 0
-    
-    for start in range(1, n + 1):
-        sum_value = 0
-        current = start
-        
-        while sum_value < n:
-            sum_value += current
-            current += 1
-        
-        if sum_value == n:
-            count += 1
-    
-    return count
+    answer = 0
+    for i in range(1, n + 1):
+        if n % i == 0 and i % 2 == 1:
+            answer+=1
+    return answer
