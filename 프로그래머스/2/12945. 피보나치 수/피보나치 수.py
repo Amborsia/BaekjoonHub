f4d12e4bf24e739f6bc799639dca7e1ab0af319c
남홍근 <ghdrms1220@naver.com>
2026-03-05 16:51:02 +0900

    [level 2] Title: 피보나치 수, Time: 560.41 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,7 @@
 def solution(n):
-    dp = [0]*(n+1)
-    dp[1]=1
-    dp[2] = 2
-    if n<=2:
-        return dp[n]
+    answer = 0
+    dp = [0] * (n+1)
+    dp[1] = 1
     for i in range(2,n+1):
-        dp[i] = (dp[i-2]+dp[i-1])%1234567
-    return dp[n]
+        dp[i] = dp[i-1]+dp[i-2]
+    return dp[n]%1234567
