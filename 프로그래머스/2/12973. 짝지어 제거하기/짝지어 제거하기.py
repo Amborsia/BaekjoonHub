d344e63a155486d6ca8ea52fac84414b4bb2edf2
남홍근 <ghdrms1220@naver.com>
2026-03-05 16:03:32 +0900

    [level 2] Title: 짝지어 제거하기, Time: 90.30 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,10 +1,9 @@
 def solution(s):
-    stack = []
-
-    for char in s:
-        if stack and stack[-1] == char:
-            stack.pop()
+    lit = []
+    for i in s:
+        if lit and lit[-1] == i:
+            lit.pop()
         else:
-            stack.append(char)
+            lit.append(i)
 
-    return 1 if not stack else 0
+    return 1 if not lit else 0
