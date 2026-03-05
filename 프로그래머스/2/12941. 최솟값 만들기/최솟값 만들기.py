93cd68a80a2ad7b4c455bf3f057075a5998ff178
남홍근 <ghdrms1220@naver.com>
2026-03-05 15:02:50 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.38 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,9 +1,10 @@
-
 def solution(A,B):
     answer = 0
     A.sort()
-    B.sort(reverse = True)
-    for i in range(len(A)):
-        answer+= A[i]*B[i]
+    B.sort(reverse=True)
+    
+    for a,b in zip(A,B):
+        answer += a*b
+
 
     return answer
